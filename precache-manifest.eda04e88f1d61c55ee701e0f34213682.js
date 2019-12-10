3ff2029bc551feb045ab706c283b1b36f2907c4b
enhao25 <larloller@gmail.com>
2019-12-10 22:37:25 +0800

    Updates

---
+++ precache-manifest.eda04e88f1d61c55ee701e0f34213682.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff087eba3cad3678a5f372db61ca7fe",
+    "url": "/catfriends/index.html"
+  },
+  {
+    "revision": "402a907179970a36b470",
+    "url": "/catfriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5077b43ec877b1c723b1",
+    "url": "/catfriends/static/css/main.bf208bdd.chunk.css"
+  },
+  {
+    "revision": "402a907179970a36b470",
+    "url": "/catfriends/static/js/2.ee428d40.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/catfriends/static/js/2.ee428d40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5077b43ec877b1c723b1",
+    "url": "/catfriends/static/js/main.ea1143c1.chunk.js"
+  },
+  {
+    "revision": "3d6ec6dd13769e785b49",
+    "url": "/catfriends/static/js/runtime-main.0ea3b7d8.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/catfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
